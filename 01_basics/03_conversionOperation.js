ea923e341207cbb8cf1427f4775ba09102ca7595
shrutiijainn11 <158989102+shrutiijainn11@users.noreply.github.com>
2024-03-24 15:58:32 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber) //NaN
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) //true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
