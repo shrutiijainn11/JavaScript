7d534319a6c0a889d777e04727885c1f47aaebff
shrutiijainn11 <158989102+shrutiijainn11@users.noreply.github.com>
2024-03-24 16:22:48 +0000

    datatype summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -7,3 +7,15 @@ let name = "Shruti"
 let age = 18
 let isLoggedIn = false
 
+let bigNumber = 27637812648178409841n //bigInt
+
+const fruits = ["apple", "mango", "banana"]
+
+let myObj = {
+    name: "Shruti",
+    age: 20
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
