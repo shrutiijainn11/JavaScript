e613916ec5f105656a58cf9d121eabd1b8ddc72a
shrutiijainn11 <158989102+shrutiijainn11@users.noreply.github.com>
2024-03-24 16:08:42 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,20 @@ console.log(booleanIsLoggedIn) //true
 let someNumber = 33
 
 let stringNumber = String(someNumber)
+
+/***************Operations***************/
+
+let value = 3
+let negvalue = -value
+
+let str1 = "hello"
+let str2 = " shruti"
+let str3 = str1+str2
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+console.log(+true) //1
+console.log(+"")//0
